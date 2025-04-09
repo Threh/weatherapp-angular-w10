fcd2c95f01afd9b6fa3a430ba61b823db380e3d6
Threh <93963865+Threh@users.noreply.github.com>
2025-04-09 09:36:01 +0700

    Add files via upload

---
+++ weatherapp - angular - w10/src/app/shared/directives/highlight-temp.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HighlightTempDirective } from './highlight-temp.directive';
+
+describe('HighlightTempDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighlightTempDirective();
+    expect(directive).toBeTruthy();
+  });
+});
