1ded6721825ea881214f21c477029eeec23976ac
Threh <93963865+Threh@users.noreply.github.com>
2025-05-16 15:35:15 +0700

    Delete weatherapp - angular - w10 directory

--- weatherapp - angular - w10/src/app/shared/directives/highlight-temp.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighlightTempDirective } from './highlight-temp.directive';
-
-describe('HighlightTempDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighlightTempDirective();
-    expect(directive).toBeTruthy();
-  });
-});
